a4a45c9d1ccd8702dbe954c08fefa66942fce149
Harsh Kushwaha <harshkushwaha508@gmail.com>
2026-01-27 19:58:42 +0530

    first commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://emldhzqpbpudzgyvjifp.supabase.co';
+  static const supabaseAnonKey =
+      'sb_publishable_Tln_fDwWTelb22EG1MF4xA_yEo918AW';
+}
