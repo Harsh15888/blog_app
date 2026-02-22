b7f3fc55b06f1fdd976b90a85a8f70fc0921ada8
Harsh Kushwaha <harshkushwaha508@gmail.com>
2026-02-22 18:27:57 +0530

    updated supabase anon key

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
   static const supabaseUrl = 'https://emldhzqpbpudzgyvjifp.supabase.co';
   static const supabaseAnonKey =
-      'sb_publishable_Tln_fDwWTelb22EG1MF4xA_yEo918AW';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtbGRoenFwYnB1ZHpneXZqaWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1MTUzMTcsImV4cCI6MjA4NTA5MTMxN30.Xc_OYbv9hzb0kMZiv1kURFZz4jkJx3XRTmNn-Jw7gU4';
 }
